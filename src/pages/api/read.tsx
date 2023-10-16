302dfdb06fd7986b7bea721fd87f03e7804094c8
[SW4/2반] 노은탁 <popcron13@gmail.com>
2023-10-16 18:27:47 +0900

    fix / 서버 에러가 어떻게 발생하는지 확인하기 위한 커밋.

--- src/pages/api/read.tsx
+++ src/pages/api/read.tsx
@@ -5,7 +5,7 @@ export default function read(req: NextApiRequest, res: NextApiResponse) {
   db.query('SELECT * FROM tdcnext.todos', function (err: any, result: any) {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'DB 오류' });
+      res.status(500).json(err);
     } else {
       res.json(result);
     }
